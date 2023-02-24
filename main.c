99af3b9483453581db85bb61f2c88a9abc10badf
Clary254 <clarencemugailwa@gmail.com>
2023-02-24 01:13:38 -0800

    get basic working shell

--- main.c
+++ main.c
@@ -1,20 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "shell.h"
-#include <string.h>
+
 /**
-  * main - entry point of the program
-  * @ac: count of argumnets passed in
-  * @av: array of arguments paased in
-  * @env: array of environment variables
-  * Return: always 0 (success);
-  */
-
-int main(int ac, char **av, char **env)
+ * main - entry point of the shell
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	main_loop(env, av);
-	return (0);
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
